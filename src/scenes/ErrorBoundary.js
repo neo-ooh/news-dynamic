853923a054267faf084df9eb7620673faed4cbbf
Valentin Dufois <valentin@dufois.fr>
2019-07-30 18:11:41 +0200

    Updating Errorboundaries style

--- src/scenes/ErrorBoundary.js
+++ src/scenes/ErrorBoundary.js
@@ -4,14 +4,11 @@ import Error from './ErrorBoundary/Error'
 class ErrorBoundary extends Component {
   constructor (props) {
     super(props)
-    this.state = { hasError: false, msg: '' }
-  }
-
-  static getDerivedStateFromError (error) {
-    console.log("getDerivedStateFromError", error)
-
-    // Display fallback UI
-    return { hasError: true, error: error }
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null,
+    }
   }
 
   componentDidCatch (error, info) {
@@ -24,7 +21,6 @@ class ErrorBoundary extends Component {
   render () {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      console.log(this.state.errorInfo)
       return <Error
         message={ this.state.error.toString() }
         stack={ this.state.errorInfo.componentStack }
--- src/scenes/ErrorBoundary/Error.js
+++ src/scenes/ErrorBoundary/Error.js
@@ -5,9 +5,9 @@ export default class Error extends Component {
     if(process.env.REACT_APP_ENV !== 'production') {
       return (
         <section id="error-screen">
-          <h1>Weather Dynamic</h1>
+          <h1>News Dynamic</h1>
           <h3>An error as occurred</h3>
-          <h6>v0.403 - { window.location.href }</h6>
+          <h6>{ window.location.href }</h6>
           <h5>{ this.props.message }</h5>
           <h5 className="stack">{ this.props.stack || '' }</h5>
         </section>
