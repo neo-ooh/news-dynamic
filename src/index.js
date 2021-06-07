0bb86f289f33bac7302de1939fd394a5bb386c4d
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-06-07 14:19:30 -0400

    WiP 2021-06-07

--- src/index.js
+++ src/index.js
@@ -1,44 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import ErrorBoundary from './scenes/ErrorBoundary'
-import App from './App'
-
-import * as serviceWorker from './serviceWorker'
+// Log Aggregation
+import { init as initApm } from '@elastic/apm-rum';
 
 // ENV
-import {dotenvConfig} from 'dotenv'
+import { config as dotenvConfig } from 'dotenv';
+import React                      from 'react';
+import ReactDOM                   from 'react-dom';
+import App                        from './App';
 
-// Log Aggregation
-import { init as initApm } from '@elastic/apm-rum'
+import ErrorBoundary from './scenes/ErrorBoundary';
+
+import * as serviceWorker from './serviceWorker';
 
 // Init APM for User Experience Tracking
 initApm({
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: 'news-dynamic',
+  serviceName   : 'news-dynamic',
   serviceVersion: process.env.REACT_APP_GIT_SHA,
 
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'https://metrics.neo-ooh.info/',
+  serverUrl       : 'https://metrics.neo-ooh.info/',
   verifyServerCert: false,
 
   // Set service version (required for sourcemap feature)
   environment: process.env.NODE_ENV,
 
   breakdownMetrics: true,
-})
+});
 
-dotenvConfig()
+dotenvConfig();
 
 //
 // GO REACT
 ReactDOM.render(
   <ErrorBoundary>
     <App/>
-  </ErrorBoundary>, document.getElementById('root'))
+  </ErrorBoundary>, document.getElementById('root'));
 
 // Register, or not if dev, the service worker
-if(process.env.REACT_APP_ENV === 'development') {
+if (process.env.REACT_APP_ENV === 'development') {
   serviceWorker.unregister();
 } else {
   serviceWorker.register();
